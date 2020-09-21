cec86ec387fbcfc62a376f92969a89bab1c8e8a3
aimee-liang <aimeeeliang@gmail.com>
2020-09-21 19:05:46 -0400

    Models and routes

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,25 @@
 class Destination < ApplicationRecord
+    has_many :posts
+    has_many :bloggers, through: :posts
+
+    def total_likes
+        self.posts.sum{ |post| post.likes}
+    end
+
+    def featured_post
+        if self.total_likes == 0
+            "This destination's posts have no likes!"
+        else
+            self.most_liked_post
+        end
+    end
+
+    def most_liked_post
+        self.posts.max_by{|post| post.likes}
+    end
+
+    def average_blogger_age
+        self.bloggers.uniq.sum{|blogger| blogger.age} / self.bloggers.uniq.count
+    end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
+    belongs_to :bloggers
+    belongs_to :destinations
 
+    validates :title, presence: true
+    validates :content, length: {minimum: 101}
 end
